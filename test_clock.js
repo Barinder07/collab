47a16ed654611cd8e3af550a16ca9bb38e74fbe9
Barinder <barinderkumar99@gmail.com>
2017-11-19 15:46:07 +0530

    code clean up

--- test_clock.js
+++
@@ -1,6 +0,0 @@
-var doSomethingPeriodic = function(){
-    var dt = new Date();
-    console.log("[TEST-CLOCK] doing something at: " + dt);
-}
-setInterval(doSomethingPeriodic, 6000 * 5); // run every 5 mins
-
